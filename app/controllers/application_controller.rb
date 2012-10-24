fac0483a11a046a351085e13ec01496564524f5c
BenStW <benedikt@startwork.in>
2012-10-24 19:25:42 +0200

    removed comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,28 +5,8 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
 
-  # before_filter :authenticate_tenant!
-   
- # def authenticate_tenant!
- #   puts params.to_yaml
- #   if params[:user] and params[:user][:email]
- #      user = User.unscoped.find_by_email(params[:user][:email])
- #      puts "CLIENT: #{user.client}"
- #      set_current_tenant(user.client)
- #   end
- #   unless authenticate_user!
- #        flash[:notice] = "NOPE!!!"
- #        return false
- #   end
- #   set_current_tenant(current_user.client)
- #   true
- # end
- # 
-
   def find_the_current_tenant
-#    debugger
     if current_user
-      #ActsAsTenant.current_tenant(current_user.client)
       set_current_tenant(current_user.client)
     else
       set_current_tenant(nil)
